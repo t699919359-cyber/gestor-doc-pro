5e4accdad2214a877a47aa8f370a24152bcf08af
t699919359-cyber <t699919359@gmail.com>
2025-12-11 00:52:37 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,30 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import App from './App';
+
+//const rootElement = document.getElementById('root');
+//if (!rootElement) {
+  //throw new Error("Could not find root element to mount to");
+//}
+
+//const root = ReactDOM.createRoot(rootElement);
+//root.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>
+//);
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Aplicación from './Aplicación';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("No se pudo encontrar el elemento raíz para montar");
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Aplicación />
   </React.StrictMode>
 );
